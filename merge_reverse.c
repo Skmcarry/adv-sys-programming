febfee9713b0f13039875fd37a6b278f14b27ffb
kkw0506 <mnblkjpoiuh@Naver.com>
2016-10-23 20:27:51 +0900

    Add files via upload

---
+++ merge_reverse.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+
+int readaline_and_reverse_out(FILE *fin, FILE *fout);
+
+int
+main(int argc, char *argv[])
+{
+    FILE *file1, *file2, *fout;
+    int eof1 = 0, eof2 = 0;
+    long line1 = 0, line2 = 0, lineout = 0;
+    struct timeval before, after;
+    int duration;
+    int ret = 1;
+
+    if (argc != 4) {
+        fprintf(stderr, "usage: %s file1 file2 fout\n", argv[0]);
+        goto leave0;
+    }
+    if ((file1 = fopen(argv[1], "rt")) == NULL) {
+        perror(argv[1]);
+        goto leave0;
+    }
+    if ((file2 = fopen(argv[2], "rt")) == NULL) {
+        perror(argv[2]);
+        goto leave1;
+    }
+    if ((fout = fopen(argv[3], "wt")) == NULL) {
+        perror(argv[3]);
+        goto leave2;
+    }
+    
+    gettimeofday(&before, NULL);
+    do {
+        if (!eof1) {
+            if (!readaline_and_reverse_out(file1, fout)) {
+                line1++; lineout++;
+            } else
+                eof1 = 1;
+        }
+        if (!eof2) {
+            if (!readaline_and_reverse_out(file2, fout)) {
+                line2++; lineout++;
+            } else
+                eof2 = 1;
+        }
+    } while (!eof1 || !eof2);
+    gettimeofday(&after, NULL);
+    
+    duration = (after.tv_sec - before.tv_sec) * 1000000 + (after.tv_usec - before.tv_usec);
+    printf("Processing time = %d.%06d sec\n", duration / 1000000, duration % 1000000);
+    printf("File1 = %ld, File2= %ld, Total = %ld Lines\n", line1, line2, lineout);
+    ret = 0;
+    
+leave3:
+    fclose(fout);
+leave2:
+    fclose(file2);
+leave1:
+    fclose(file1);
+leave0:
+    return ret; 
+}
+
+/* Read a line from fin and write it to fout */
+/* return 1 if fin meets end of file */
+int
+readaline_and_reverse_out(FILE *fin, FILE *fout)
+{    
+    int ch, count = 0;
+    char temp[100];
+    char tmp;
+
+    do {
+        if ((fread(&ch,1,1,fin)) < 1) {
+            if (!count)
+                return 1;
+            else {
+                temp[count]=0x0a;
+                break;
+            }
+        }
+        temp[count] = ch;
+        count++;
+    } while (ch != 0x0a);
+    
+    temp[count] = '\0';
+    
+    int n= strlen(temp);
+    
+    for(n;n>=0;n--){
+        tmp=temp[n];
+        fwrite(&tmp,1,1,fout);
+    }
+    return 0;
+}
+
